857179e69efdbc8556be611ac3426fbde3b97f98
12rambau <pierrick.rambaud49@gmail.com>
2022-02-21 19:20:03 +0000

    add a dynamic select

--- component/tile/map_tile.py
+++ component/tile/map_tile.py
@@ -9,4 +9,14 @@ class MapTile(sw.Tile):
         # set the map in the center
         self.map = cw.AlertMap()
 
+        # I decided to set the widgets here instead of in the map to avoid
+        # complexity with model sharing
+        self.select = cw.DynamicSelect()
+
+        # place them in the map
+        self.map.add_widget_as_control(self.select, "topright", True)
+
+        # link to the btn for activation
+        self.map.navigate_btn.on_click(lambda *args: self.select.toggle_viz())
+
         super().__init__(id_="map_tile", title="Map tile", inputs=[self.map])
--- component/widget/__init__.py
+++ component/widget/__init__.py
@@ -1,1 +1,2 @@
 from .alert_map import *
+from .dynamic_select import *
