ee6b31543ce7580ae4562b7a6737340dd02a46c4
12rambau <pierrick.rambaud49@gmail.com>
2022-05-13 15:37:55 +0000

    adapt zoom level to the position in the card

--- component/tile/setting_tile.py
+++ component/tile/setting_tile.py
@@ -11,13 +11,17 @@ from .planet_view import PlanetView
 class SettingTile(sw.Card):
     def __init__(self, map_):
 
+        # save the map as a member
+        # for the zoom change option
+        self.map = map_
+
         # create the 2 subtiles
         self.aoi_view = AoiView(map_=map_)
         self.alert_view = AlertView(self.aoi_view.model, map_)
         self.planet_view = PlanetView(self.alert_view.alert_model)
 
         # set them into a tab widget
-        tabs = sw.Tabs(
+        self.tabs = sw.Tabs(
             fixed_tabs=True,
             v_model=0,
             children=[
@@ -43,7 +47,7 @@ class SettingTile(sw.Card):
 
         # create the card
         super().__init__(
-            children=[title, tabs, contents],
+            children=[title, self.tabs, contents],
             max_height="80vh",  # prevent from going outside the map
             min_width="30vw",
             max_width="30vw",
@@ -52,8 +56,9 @@ class SettingTile(sw.Card):
 
         # add js behaviour
         self.close.on_event("click", lambda *args: self.toggle_viz())
-        link((tabs, "v_model"), (contents, "v_model"))
+        link((self.tabs, "v_model"), (contents, "v_model"))
         self.alert_view.alert_model.observe(self._hide_on_success, "gdf")
+        self.tabs.observe(self.update_zoom, "v_model")
 
     def _hide_on_success(self, change):
         """hide the tile if alerts are loaded on the map"""
@@ -62,3 +67,25 @@ class SettingTile(sw.Card):
             self.hide()
 
         return self
+
+    def update_zoom(self, change):
+        """
+        Update the zoom based on the selected panel
+        - If no AOI is selected, we do nothing
+        - AOI and planet we zoom on the aoi
+        - alert we zoom on the world to see the alert footprint
+        """
+
+        # do nothing if no AOI
+        if self.aoi_view.model.gdf is None:
+            return
+
+        # aoi selection and planet
+        if self.tabs.v_model == 0 or self.tabs.v_model == 1:
+            self.map.zoom_ee_object(self.aoi_view.model.feature_collection.geometry())
+        # alert selection
+        elif self.tabs.v_model == 2:
+            self.map.zoom = 2
+            self.map.center = [0, 0]
+
+        return
