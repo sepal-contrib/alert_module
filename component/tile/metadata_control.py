2cf8ce69bf4cb5fda066bb7a66823604afb2ec1d
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-05-11 12:13:32 +0200

    fix: solve navigation issue

--- component/tile/metadata_control.py
+++ component/tile/metadata_control.py
@@ -222,7 +222,7 @@ class MetadataView(sw.Card):
         # it from the v_model which is using yyyy-mm-dd
         date_pattern = re.compile(r"(\d{4})-(0[1-9]|1[0-2])-([0-2][1-9]|3[01])")
         match = date_pattern.match(change["new"])
-        year, month, day = match.groups if match else ("1970", "01", "01")
+        year, month, day = match.groups() if match else ("1970", "01", "01")
         date = datetime(int(year), int(month), int(day))
         date = int(date.strftime("%j")) / 1000 + date.year
 
